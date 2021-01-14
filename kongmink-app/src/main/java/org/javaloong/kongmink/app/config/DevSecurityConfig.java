d7bfb71737a2dcfdd2f4ea9ce7e954478d232cd7
tutunannan <bd66_6@hotmail.com>
2021-01-14 17:31:54 +0800

    feat: add security config

---
+++ kongmink-app/src/main/java/org/javaloong/kongmink/app/config/DevSecurityConfig.java
@@ -0,0 +1,39 @@
+package org.javaloong.kongmink.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Profile("dev")
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class DevSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin").password("{noop}admin").authorities("ADMIN")
+            .and()
+            .withUser("user").password("{noop}user").authorities("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+            .httpBasic()
+                .disable()
+            .formLogin()
+                .permitAll()
+                .and()
+            .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+}
